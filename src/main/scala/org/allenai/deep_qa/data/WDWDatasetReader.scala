601a5baa63da97a406ef71cc46a24416003b21eb
Nelson Liu <nelson.liu.2009@gmail.com>
2017-01-05 21:54:55 +0000

    add WDWDatasetReader to parse XML WDW dataset

---
+++ src/main/scala/org/allenai/deep_qa/data/WDWDatasetReader.scala
@@ -0,0 +1,38 @@
+package org.allenai.deep_qa.data
+
+import com.mattg.util.FileUtil
+
+import scala.collection.mutable
+import scala.xml.XML
+
+
+class WDWDatasetReader(fileUtil: FileUtil) extends DatasetReader[WDWInstance] {
+  override def readFile(filename: String): Dataset[WDWInstance] = {
+    val xml = XML.loadFile(filename)
+    val instanceTuples = for {
+      mc <- xml \ "mc"
+      question_node = mc \ "question"
+      question_text = question_node.text
+
+      leftContext_node = question_node \ "leftcontext"
+      leftContext = leftContext_node.text
+
+      rightContext_node = question_node \ "rightcontext"
+      rightContext = rightContext_node.text
+
+      passage_node = mc \ "contextart"
+      passage = passage_node.text
+
+      answer_nodes = mc \ "choice"
+      answers = answer_nodes.map((answer: xml.Node) => answer.text)
+
+      label_nodes = answer_nodes.filter((answer: xml.Node) => (answer \ "@correct").text == "true")
+      label = Integer.parseInt((label_nodes \ "@idx").text)
+    } yield (passage, leftContext, rightContext, answers, label)
+
+    val instances = instanceTuples.map { case (passage, leftContext, rightContext, answers, label) => {
+      WDWInstance(passage, leftContext, rightContext, answers, label)
+    }}
+    Dataset(instances)
+  }
+}
