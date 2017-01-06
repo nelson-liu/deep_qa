b16c0fff191ff70d0a09fb9cf909475b15d79c69
Nelson Liu <nelson.liu.2009@gmail.com>
2017-01-06 00:00:16 +0000

    add wdw properties for experimentation

---
+++ src/main/scala/org/allenai/deep_qa/experiments/datasets/WDW.scala
@@ -0,0 +1,32 @@
+package org.allenai.deep_qa.experiments.datasets
+
+import org.json4s._
+import org.json4s.JsonDSL._
+
+/**
+ * This object contains a bunch of JValue specifications for WDW data files.
+ */
+object WDWDatasets {
+
+  def wdwFile(wdwDir: String, split: String): JValue = {
+    val outputDirectory = wdwDir + "processed/"
+    val inputFile = wdwDir + s"${split}.xml"
+    val outputFiles = Seq(outputDirectory + s"${split}.tsv")
+    ("sentence producer type" -> "dataset reader") ~
+    ("reader" -> "wdw") ~
+    ("input file" -> inputFile) ~
+    ("output files" -> outputFiles)
+  }
+
+  def wdwDataset(wdwDir: String, split: String): JValue = {
+    val file = wdwFile(wdwDir, split)
+    ("data files" -> List(file))
+  }
+
+  val baseDir = "/efs/data/dlfa/who_did_what/Relaxed/"
+
+  val trainFile = wdwFile(baseDir, "train")
+  val train = wdwDataset(baseDir, "train")
+  val devFile = wdwFile(baseDir, "dev")
+  val dev = wdwDataset(baseDir, "dev")
+}
