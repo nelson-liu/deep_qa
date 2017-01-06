984d9de295ff01d0fff9b6953cc3751e63d5bbec
Nelson Liu <nelson.liu.2009@gmail.com>
2017-01-06 19:26:29 +0000

    add optional parameter for using relaxed dataset

---
+++ src/main/scala/org/allenai/deep_qa/experiments/datasets/WhoDidWhat.scala
@@ -0,0 +1,42 @@
+package org.allenai.deep_qa.experiments.datasets
+
+import org.json4s._
+import org.json4s.JsonDSL._
+
+/**
+ * This object contains a bunch of JValue specifications for WDW data files.
+ */
+object WhoDidWhatDatasets {
+
+  def WhoDidWhatFile(wdwDir: String, split: String, relaxed: Boolean = false): JValue = {
+    var outputSubdir = "processed/Strict/"
+    if (relaxed){
+      if (split != "train"){
+        // relaxed is for training only
+        throw new IllegalArgumentException("Relaxed dataset can only be used for train.")
+      }
+      outputSubdir = "processed/Relaxed/"
+    }
+    val outputDirectory = wdwDir + outputSubdir
+    val inputFile = wdwDir + s"${split}.xml"
+    val outputFiles = Seq(outputDirectory + s"${split}.tsv")
+    ("sentence producer type" -> "dataset reader") ~
+    ("reader" -> "wdw") ~
+    ("input file" -> inputFile) ~
+    ("output files" -> outputFiles)
+  }
+
+  def WhoDidWhatDataset(wdwDir: String, split: String, relaxed: Boolean = false): JValue = {
+    val file = WhoDidWhatFile(wdwDir, split, relaxed)
+    ("data files" -> List(file))
+  }
+
+  val baseDir = "/efs/data/dlfa/who_did_what/"
+
+  val trainFile = WhoDidWhatFile(baseDir, "train")
+  val train = WhoDidWhatDataset(baseDir, "train")
+  val devFile = WhoDidWhatFile(baseDir, "dev")
+  val dev = WhoDidWhatDataset(baseDir, "dev")
+  val relaxedTrainFile = WhoDidWhatFile(baseDir, "train", true)
+  val relaxedTrrain = WhoDidWhatDataset(baseDir, "train", true)
+}
