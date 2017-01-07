e225b13b18b18b07872ece207560f27b5ab34be8
Nelson Liu <nelson.liu.2009@gmail.com>
2017-01-07 21:55:20 +0000

    change SquadInstance to subclass QuestionPassageInstance

--- src/main/python/deep_qa/data/instances/squad_instance.py
+++ src/main/python/deep_qa/data/instances/squad_instance.py
@@ -1,17 +1,17 @@
-from typing import Tuple
+from typing import Tuple, List
 
 from overrides import overrides
 
-from .sentence_pair_instance import IndexedSentencePairInstance, SentencePairInstance
+from .question_passage_instance import IndexedQuestionPassageInstance, QuestionPassageInstance
 from ..data_indexer import DataIndexer
 from ..tokenizer import tokenizers, Tokenizer
 
 
-class SquadInstance(SentencePairInstance):
+class SquadInstance(QuestionPassageInstance):
     """
-    A SquadInstance is a SentencePairInstance that represents a (question, passage) pair from the
+    A SquadInstance is a QuestionPassageInstance that represents a (question, passage) pair from the
     Stanford Question Answering dataset, with an associated label.  The main thing this class
-    handles over SentencePairInstance is the label, which is given as a span of _characters_ in the
+    handles over QuestionPassageInstance is the label, which is given as a span of _characters_ in the
     passage.  The label we are going to use in the rest of the code is a span of _tokens_ in the
     passage, so the mapping from character labels to token labels depends on the tokenization we
     did, and the logic to handle this is, unfortunately, a little complicated.  The label
@@ -27,16 +27,14 @@ class SquadInstance(SentencePairInstance):
         super(SquadInstance, self).__init__(question, passage, label, index, tokenizer)
 
     def __str__(self):
-        return 'SquadInstance(' + self.first_sentence + ', ' + self.second_sentence + ', ' + str(self.label) + ')'
+        return 'SquadInstance(' + self.qusetion_text + ', ' + self.passage_text + ', ' + str(self.label) + ')'
 
     @overrides
-    def to_indexed_instance(self, data_indexer: DataIndexer):
-        indexed_question = self._index_text(self.first_sentence, data_indexer)
-        indexed_passage = self._index_text(self.second_sentence, data_indexer)
+    def _index_label(self, label: Tuple[int, int]) -> List[int]:
         new_label = None
         if self.label is not None:
-            new_label = self.tokenizer.char_span_to_token_span(self.second_sentence, self.label)
-        return IndexedSentencePairInstance(indexed_question, indexed_passage, new_label, self.index)
+            new_label = self.tokenizer.char_span_to_token_span(self.passage_text, self.label)
+        return new_label
 
     @classmethod
     def read_from_line(cls,
