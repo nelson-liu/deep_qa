d735212c2aa52a9bde34ded782ccfb5978187778
Nelson Liu <nelson.liu.2009@gmail.com>
2017-01-27 17:39:55 +0000

    move message generation to new method

--- src/main/python/deep_qa/training/trainer.py
+++ src/main/python/deep_qa/training/trainer.py
@@ -46,21 +46,8 @@ class Trainer:
         # train but we also warn the user.
         self.preferred_backend = params.pop('preferred_backend', None)
         if self.preferred_backend and self.preferred_backend.lower() != K.backend():
-            warning_info = ("@ Preferred backend is %s, but "
-                            "current backend is %s. @" % (self.preferred_backend.lower(),
-                                                          K.backend()))
-            end_row = "@" * len(warning_info)
-            warning_row_spaces = len(warning_info) - len("@ WARNING: @")
-            left_warning_row_spaces = right_warning_row_spaces = warning_row_spaces // 2
-            if warning_row_spaces % 2 == 1:
-                # left and right have uneven spacing
-                right_warning_row_spaces += 1
-            left_warning_row = "\n@" + " " * left_warning_row_spaces
-            right_warning_row = " " * right_warning_row_spaces + "@\n"
-            warning_message = ("\n" + end_row +
-                               left_warning_row + " WARNING: " + right_warning_row +
-                               warning_info +
-                               "\n" + end_row)
+            warning_message = self._make_backend_warning(self.preferred_backend.lower(),
+                                                         K.backend())
             logger.warning(warning_message)
 
         # Upper limit on the number of training instances.  If this is set, and we get more than
@@ -536,6 +523,24 @@ class Trainer:
         model_config_file = open("%s_config.json" % (self.model_prefix), "w")
         print(model_config, file=model_config_file)
         model_config_file.close()
+
+    def _make_backend_warning(self, preferred_backend, actual_backend):
+        warning_info = ("@ Preferred backend is %s, but "
+                        "current backend is %s. @" % (preferred_backend,
+                                                      actual_backend))
+        end_row = "@" * len(warning_info)
+        warning_row_spaces = len(warning_info) - len("@ WARNING: @")
+        left_warning_row_spaces = right_warning_row_spaces = warning_row_spaces // 2
+        if warning_row_spaces % 2 == 1:
+            # left and right have uneven spacing
+            right_warning_row_spaces += 1
+        left_warning_row = "\n@" + " " * left_warning_row_spaces
+        right_warning_row = " " * right_warning_row_spaces + "@\n"
+        warning_message = ("\n" + end_row +
+                           left_warning_row + " WARNING: " + right_warning_row +
+                           warning_info +
+                           "\n" + end_row)
+        return warning_message
 
     @classmethod
     def _get_custom_objects(cls):
