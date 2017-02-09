73d44d72964f6d86c1da25c7c54a93b3af77254a
Nelson Liu <nelson.liu.2009@gmail.com>
2017-02-09 21:44:22 +0000

    add test for masked case

--- src/test/python/deep_qa_test/layers/backend/masked_batch_dot_test.py
+++ src/test/python/deep_qa_test/layers/backend/masked_batch_dot_test.py
@@ -1,6 +1,7 @@
 # pylint: disable=no-self-use,invalid-name
 import numpy
 from numpy.testing import assert_almost_equal
+import keras.backend as K
 from keras.layers import Input
 from keras.models import Model
 
@@ -18,14 +19,32 @@ class TestMaskedBatchDotLayer:
                                 [[0.3, 0.1], [0.4, 0.2], [0.8, 0.1]]])
         tensor_b = numpy.array([[[0.2, 0.6], [0.4, 0.3], [0.5, 0.7], [0.1, .6]],
                                 [[0.2, 0.6], [0.4, 0.3], [0.5, 0.7], [0.1, .6]]])
-        adotb = model.predict([tensor_a, tensor_b])
-        assert adotb.shape == (batch_size, 3, 4)
-        assert_almost_equal(adotb, numpy.array([[[ 0.12, 0.15000001, 0.22, 0.09],
-                                                 [ 0.20000002, 0.22000001, 0.34, 0.16000001],
-                                                 [ 0.22000001, 0.35000002, 0.47, 0.14000002]],
-                                                [[ 0.12, 0.15000001, 0.22, 0.09],
-                                                 [ 0.20000002, 0.22000001, 0.34, 0.16000001],
-                                                 [ 0.22000001, 0.35000002, 0.47, 0.14000002]]]))
+        a_dot_b = model.predict([tensor_a, tensor_b])
+        assert a_dot_b.shape == (batch_size, 3, 4)
+        assert_almost_equal(a_dot_b, numpy.array([[[0.12, 0.15, 0.22, 0.09],
+                                                   [0.2, 0.22, 0.34, 0.16],
+                                                   [0.22, 0.35, 0.47, 0.14]],
+                                                  [[0.12, 0.15, 0.22, 0.09],
+                                                   [0.2, 0.22, 0.34, 0.16],
+                                                   [0.22, 0.35, 0.47, 0.14]]]))
+
+    def test_call_works_on_masked_input(self):
+        tensor_a = K.variable(numpy.array([[[0.3, 0.1], [0.4, 0.2], [0.8, 0.1]],
+                                           [[0.3, 0.1], [0.4, 0.2], [0.8, 0.1]]]))
+        print(tensor_a)
+        mask_a = K.variable(numpy.array([[1, 0, 1], [1, 1, 0]]))
+        print(mask_a)
+        tensor_b = K.variable(numpy.array([[[0.2, 0.6], [0.4, 0.3], [0.5, 0.7], [0.1, .6]],
+                                           [[0.2, 0.6], [0.4, 0.3], [0.5, 0.7], [0.1, .6]]]))
+        mask_b = K.variable(numpy.array([[0, 1, 1, 1], [1, 0, 1, 1]]))
+        masked_a_dot_b = K.eval(MaskedBatchDot()([tensor_a, tensor_b],
+                                                 [mask_a, mask_b]))
+        assert_almost_equal(masked_a_dot_b, numpy.array([[[0.0, 0.15, 0.22, 0.09],
+                                                          [0.0, 0.0, 0.0, 0.0],
+                                                          [0.0, 0.35, 0.47, 0.14]],
+                                                         [[0.12, 0.0, 0.22, 0.09],
+                                                          [0.2, 0.0, 0.34, 0.16],
+                                                          [0.0, 0.0, 0.0, 0.0]]]))
 
     def test_get_output_shape_for(self):
         # TODO(nelson): verify the correctness of this, esp. in rank 1 edge case.
