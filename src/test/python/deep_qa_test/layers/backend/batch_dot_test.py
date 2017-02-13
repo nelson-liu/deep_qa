879725ecb99b740335befa62296e60197aab6d45
Nelson Liu <nelson.liu.2009@gmail.com>
2017-02-13 19:19:20 +0000

    add tests for uneven tensors that are theano compatible

--- src/test/python/deep_qa_test/layers/backend/batch_dot_test.py
+++ src/test/python/deep_qa_test/layers/backend/batch_dot_test.py
@@ -80,6 +80,70 @@ class TestBatchDotLayer(TestCase):
                                                            [1.0, 1.0, 0.0, 1.0],
                                                            [0.0, 0.0, 0.0, 0.0]]]))
 
+    def test_compute_mask_a_smaller_than_b(self):
+        batch_size = 3
+        tensor_a = numpy.random.randint(7, size=(batch_size, 5))
+        tensor_b = numpy.random.randint(7, size=(batch_size, 2, 5))
+
+        # Manually set some values to 1 here, which will be masked later
+        # (1 and not 0 so that masked values are still non-zero in the output)
+        tensor_a[0] = 0
+        tensor_b[0][1] = 0
+
+        input_tensor_a = Input(shape=(5,))
+        masked_tensor_a = Masking(mask_value=0)(input_tensor_a)
+        input_tensor_b = Input(shape=(2, 5))
+        masked_tensor_b = Masking(mask_value=0)(input_tensor_b)
+
+        a_dot_b = BatchDot()([masked_tensor_a, masked_tensor_b])
+
+        a_dot_b_mask = OutputMask()(a_dot_b)
+        model = Model(input=[input_tensor_a, input_tensor_b],
+                      output=[a_dot_b, a_dot_b_mask])
+        # a_dot_b and mask_tensor are of shape (3, 2).
+        a_dot_b_tensor, mask_tensor = model.predict([tensor_a, tensor_b])
+        # Test that the dot happened like we expected.
+        for i in range(batch_size):
+            # each dot product should be of shape (2,)
+            assert_almost_equal(a_dot_b_tensor[i],
+                                numpy.einsum("i,mi->m", tensor_a[i], tensor_b[i]))
+        # Check that the values in the output mask are 0 where the
+        # values were set to 1 above.
+        assert mask_tensor[0][0] == 0
+        assert mask_tensor[0][1] == 0
+
+    def test_compute_mask_a_larger_than_b(self):
+        batch_size = 3
+        tensor_a = numpy.random.randint(7, size=(batch_size, 2, 5))
+        tensor_b = numpy.random.randint(7, size=(batch_size, 5))
+
+        # Manually set some values to 1 here, which will be masked later
+        # (1 and not 0 so that masked values are still non-zero in the output)
+        tensor_a[0][1] = 0
+        tensor_b[0] = 0
+
+        input_tensor_a = Input(shape=(2, 5))
+        masked_tensor_a = Masking(mask_value=0)(input_tensor_a)
+        input_tensor_b = Input(shape=(5,))
+        masked_tensor_b = Masking(mask_value=0)(input_tensor_b)
+
+        a_dot_b = BatchDot()([masked_tensor_a, masked_tensor_b])
+
+        a_dot_b_mask = OutputMask()(a_dot_b)
+        model = Model(input=[input_tensor_a, input_tensor_b],
+                      output=[a_dot_b, a_dot_b_mask])
+        # a_dot_b and mask_tensor are of shape (3, 2).
+        a_dot_b_tensor, mask_tensor = model.predict([tensor_a, tensor_b])
+        # Test that the dot happened like we expected.
+        for i in range(batch_size):
+            # each dot product should be of shape (2,)
+            assert_almost_equal(a_dot_b_tensor[i],
+                                numpy.einsum("mi,i->m", tensor_a[i], tensor_b[i]))
+        # Check that the values in the output mask are 0 where the
+        # values were set to 1 above.
+        assert mask_tensor[0][0] == 0
+        assert mask_tensor[0][1] == 0
+
     def test_compute_mask_a_smaller_than_b_higher_dimension(self):
         batch_size = 3
         tensor_a = numpy.random.randint(7, size=(batch_size, 4, 5))
@@ -108,12 +172,7 @@ class TestBatchDotLayer(TestCase):
         model = Model(input=[input_tensor_a, input_tensor_b],
                       output=[a_dot_b, a_dot_b_mask])
         # a_dot_b and mask_tensor are of shape (3, 4, 2).
-        if K.backend() == "theano":
-            self.assertRaises(RuntimeError, model.predict,
-                              [tensor_a, tensor_b])
-            return
-        else:
-            a_dot_b_tensor, mask_tensor = model.predict([tensor_a, tensor_b])
+        a_dot_b_tensor, mask_tensor = model.predict([tensor_a, tensor_b])
         # Test that the dot happened like we expected.
         for i in range(batch_size):
             # each dot product should be of shape (4, 2)
@@ -178,7 +237,6 @@ class TestBatchDotLayer(TestCase):
                 # this breaks in theano, so check that an error is raised
                 self.assertRaises(RuntimeError, mbd.call,
                                   [K.ones(shape=a_shape), K.ones(shape=b_shape)])
-                # assert K.eval(mbd([K.ones(shape=a_shape), K.ones(shape=b_shape)])).shape == expected_shape
             else:
                 assert K.eval(mbd([K.ones(shape=a_shape), K.ones(shape=b_shape)])).shape == expected_shape
             assert mbd.get_output_shape_for([a_shape, b_shape]) == expected_shape
