b05b3af277df00e8c2ea51b15e9d3b89299c8a09
Nelson Liu <nelson.liu.2009@gmail.com>
2017-02-13 18:59:55 +0000

    correct batch_dot layer and add more comprehensive tests

--- src/main/python/deep_qa/layers/backend/batch_dot.py
+++ src/main/python/deep_qa/layers/backend/batch_dot.py
@@ -37,50 +37,84 @@ class BatchDot(Layer):
         a_dot_axis = K.ndim(tensor_a) - 1
         b_dot_axis = K.ndim(tensor_b) - 1
 
-        if K.ndim(tensor_a) < K.ndim(tensor_b):
-            # To simplify the logic below, we'll make sure that tensor_a is always the bigger one.
-            tensor_a, tensor_b = tensor_b, tensor_a
-            mask_a, mask_b = mask_b, mask_a
-
         if mask_a is None and mask_b is None:
             return None
         elif mask_a is None:
-            # (batch_size, a_length)
             mask_a = K.sum(K.ones_like(tensor_a), axis=-1)
         elif mask_b is None:
             # (batch_size, b_length)
-            sum_axis = -1
-            if b_dot_axis < a_dot_axis:
-                sum_axis -= 1
-            mask_b = K.sum(K.ones_like(tensor_b), axis=sum_axis)
-        # Casting masks to float since we TF would complain if we multiplied bools.
-        float_mask_a = K.cast(mask_a, 'float32')
-        float_mask_b = K.cast(mask_b, 'float32')
-
-        if b_dot_axis < a_dot_axis:
-            float_mask_b = K.expand_dims(float_mask_b, dim=-1)
-        else:
+            mask_b = K.sum(K.ones_like(tensor_b), axis=-1)
+        float_mask_a = K.cast(mask_a, "float32")
+        float_mask_b = K.cast(mask_b, "float32")
+        mask_a_dot_axis = K.ndim(mask_a) - 1
+        mask_b_dot_axis = K.ndim(mask_b) - 1
+        if b_dot_axis == a_dot_axis:
+            # tensor_a and tensor_b have the same length.
             float_mask_a = K.expand_dims(float_mask_a, dim=-1)
-            float_mask_b = K.expand_dims(float_mask_b, dim=-2)
-        # (batch_size, a_length, b_length)
-        a2b_mask = float_mask_a * float_mask_b
-        return a2b_mask
-
+            float_mask_b = K.expand_dims(float_mask_b, dim=-1)
+            final_mask = K.batch_dot(float_mask_a, float_mask_b,
+                                     axes=(a_dot_axis, b_dot_axis))
+        elif a_dot_axis < b_dot_axis:
+            # tensor_a has less dimensions than tensor_b.
+            # We tile tensor_a to have the same shape as tensor_b
+            float_mask_a = K.expand_dims(float_mask_a, dim=-1)
+            # We take the shape of mask_b as a hacky way of getting
+            # around K.int_shape() and the Theano backend.
+            tiled_float_mask_a = K.repeat_elements(float_mask_a,
+                                                   K.int_shape(mask_b)[-1],
+                                                   mask_b_dot_axis)
+            final_mask = tiled_float_mask_a * float_mask_b
+        else:
+            # tensor_a has more dimensions than tensor_b.
+            # We tile tensor_b to have the same shape as tensor_a
+            float_mask_b = K.expand_dims(float_mask_b, dim=-1)
+            # We take the shape of mask_a as a hacky way of getting
+            # around K.int_shape() and the Theano backend.
+            tiled_float_mask_b = K.repeat_elements(float_mask_b,
+                                                   K.int_shape(mask_a)[-1],
+                                                   mask_a_dot_axis)
+            final_mask = float_mask_a * tiled_float_mask_b
+        return final_mask
 
     @overrides
     def get_output_shape_for(self, input_shape):
-        # This assumes that we do the dot product over the last dimension, so this
-        # will have to change if that assumption changes in masked_batch_dot.
-        a_out_shape = tuple([input_shape[0][i] for i in range(0, len(input_shape[0]) - 1)])
-        b_out_shape = tuple([input_shape[1][i] for i in range(1, len(input_shape[1]) - 1)])
-        final_out_shape = a_out_shape + b_out_shape
+        tensor_a_shape, tensor_b_shape = input_shape
+        a_dot_axis = len(tensor_a_shape) - 1
+        b_dot_axis = len(tensor_b_shape) - 1
+        if b_dot_axis < a_dot_axis:
+            tensor_b_shape += (1,)
+        if b_dot_axis > a_dot_axis:
+            tensor_a_shape += (1,)
+
+        # This assumes that we do the dot product over the last dimension
+        final_out_shape = []
+        for i in range(0, len(tensor_a_shape)):
+            if i != a_dot_axis:
+                final_out_shape.append(tensor_a_shape[i])
+        for i in range(len(tensor_b_shape)-2, len(tensor_b_shape)):
+            if i != b_dot_axis and i != 0:
+                final_out_shape.append(tensor_b_shape[i])
+        if b_dot_axis != a_dot_axis:
+            # remove the 1 we inserted
+            final_out_shape.pop(a_dot_axis)
         if len(final_out_shape) == 1:
-            final_out_shape += (1,)
-        return final_out_shape
+            final_out_shape.append(1)
+        return tuple(final_out_shape)
 
     @overrides
     def call(self, x, mask=None):
         tensor_a, tensor_b = x
+        if (K.ndim(tensor_a) > 3 or K.ndim(tensor_b) > 3) and K.backend() == 'theano':
+            raise RuntimeError("K.batch_dot() in theano is broken for tensors with more than"
+                               " three dimensions.  Use tensorflow instead.")
+
         a_dot_axis = K.ndim(tensor_a) - 1
         b_dot_axis = K.ndim(tensor_b) - 1
-        return K.batch_dot(tensor_a, tensor_b, axes=(a_dot_axis, b_dot_axis))
+        if a_dot_axis > b_dot_axis:
+            tensor_b = K.expand_dims(tensor_b, dim=-1)
+        if a_dot_axis < b_dot_axis:
+            tensor_a = K.expand_dims(tensor_a, dim=-1)
+        a_dot_b = K.batch_dot(tensor_a, tensor_b, axes=(a_dot_axis, b_dot_axis))
+        if a_dot_axis != b_dot_axis:
+            a_dot_b = K.squeeze(a_dot_b, axis=a_dot_axis)
+        return a_dot_b
