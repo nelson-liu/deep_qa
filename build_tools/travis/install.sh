c66dd11b97d1ec6e5c1e13f6e62338c616a22344
Nelson Liu <nelson.liu.2009@gmail.com>
2017-02-23 19:42:06 +0000

    pin python to 3.5.2 on travis if testing pylint

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -25,12 +25,18 @@ if [[ ! -d /home/travis/miniconda3 ]]
     export PATH=/home/travis/miniconda3/bin:$PATH
     echo $PATH
     conda update --yes conda
-    conda create -n testenv --yes python=3.5
+    # If we are running pylint, use Python 3.5.2 due to
+    # bug in pylint. https://github.com/PyCQA/pylint/issues/1295
+    if [[ "$RUN_PYLINT" == "true" ]]; then
+        conda create -n testenv --yes python=3.5.2
+    else
+        conda create -n testenv --yes python=3.5
+    fi
 fi
 cd ..
 popd
 
-# create a python 3.5 environment
+# Activate the python environment we created.
 source activate testenv
 
 # Install requirements via pip in our conda environment
