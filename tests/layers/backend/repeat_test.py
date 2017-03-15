24e1d8417d01dc550de5d8ab168b90c212cb7887
Nelson Liu <nelson.liu.2009@gmail.com>
2017-03-15 02:54:47 +0000

    Repeat also can not take a list as input, it must be a single tensor

--- tests/layers/backend/repeat_test.py
+++ tests/layers/backend/repeat_test.py
@@ -12,10 +12,11 @@ class TestRepeatLayer:
         input_length = 3
         repetitions = 4
         input_layer = Input(shape=(input_length,), dtype='float32')
-        repeat_output = Repeat(axis=1, repetitions=repetitions)([input_layer])
+        repeat_output = Repeat(axis=1, repetitions=repetitions)(input_layer)
         model = Model(input=[input_layer], output=[repeat_output])
         input_tensor = numpy.asarray([[2, 5, 3], [-1, -4, -2]])
         repeat_tensor = model.predict([input_tensor])
+        print(repeat_tensor)
         assert repeat_tensor.shape == (batch_size, repetitions, input_length)
         for i in range(repetitions):
             numpy.testing.assert_almost_equal(repeat_tensor[:, i, :], [[2, 5, 3], [-1, -4, -2]])
